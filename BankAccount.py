73f7726021da07027f8035ac7ad9397ec0a5c5d9
mcdott <111994803+mcdott@users.noreply.github.com>
2022-10-27 20:40:30 -0400

    Added example code to demonstrate BankAccount methods

--- BankAccount.py
+++ BankAccount.py
@@ -25,4 +25,26 @@ class BankAccount:
 
     def print_statement(self):
         last_4_digits_of_account_number = self.account_number[4:8]
-        print(f'{self.full_name} \nAccount No.: ****{last_4_digits_of_account_number} \nBalance: {self.balance} ')
+        print(f'{self.full_name} \nAccount No.: ****{last_4_digits_of_account_number} \nBalance: ${self.balance} ')
+
+# Instantiate 3 bank accounts
+investment_account = BankAccount('Mitchell Hudson', '03141592', 0)
+chequing = BankAccount('Sam Bologna', '54789384', 467.92)
+savings = BankAccount('Pia Singh', '34508962', 1673.07)
+
+chequing.get_balance()
+chequing.withdraw(50)
+chequing.deposit(12.50)
+chequing.get_balance()
+
+savings.get_balance()
+savings.add_interest()
+savings.print_statement()
+
+investment_account.deposit(400000)
+investment_account.print_statement()
+investment_account.add_interest()
+investment_account.print_statement()
+investment_account.withdraw(150)
+investment_account.print_statement()
+
